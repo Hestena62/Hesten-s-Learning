e195f8aa371c5655c0bae39fb23fd5919c5452d1
Hestena62 <179787676+Hestena62@users.noreply.github.com>
2025-11-05 00:45:19 -0500

    update

--- research/index.php
+++ research/index.php
@@ -14,7 +14,7 @@
   // --- 1. INCLUDE HEADER ---
   // This file contains the DOCTYPE, <head>, and main navigation <header>
   // It uses all the $page... variables defined above
-  include '..\src\header.php';
+  include 'src/header.php';
 ?>
 
 <!--
